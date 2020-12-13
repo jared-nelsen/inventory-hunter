b0fb94bf3406abd70c5584c72d47460aa0a78257
Jared Nelsen <jnelsen788@gmail.com>
2020-12-13 12:21:41 +0000

    Removed run

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker pull ericjmarti/inventory-hunter:latest
-
-./docker_run.bash -c ./config/all_3090.yaml -a discord -w https://discordapp.com/api/webhooks/787638741678161950/5235YnavaPwW8DfTPVeBLeIftNTF2P-SNzo5NKWPPAXX0pOiLk6jEHaQoKHe2kR0_i_X
