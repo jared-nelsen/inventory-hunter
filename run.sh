9be757145f0c9d3e32967a3ec452bed5785c7d68
Jared Nelsen <jnelsen788@gmail.com>
2020-12-13 07:05:04 -0500

    removed clone and cd

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/jared-nelsen/inventory-hunter
-cd inventory-hunter
 docker pull ericjmarti/inventory-hunter:latest
 
 ./docker_run.bash -c ./config/all_3090.yaml -a discord -w https://discordapp.com/api/webhooks/787638741678161950/5235YnavaPwW8DfTPVeBLeIftNTF2P-SNzo5NKWPPAXX0pOiLk6jEHaQoKHe2kR0_i_X
